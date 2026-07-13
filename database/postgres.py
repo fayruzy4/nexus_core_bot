26cc82fc02a489958da724c2ebc36e15526d534b
fayruzy4 <najibfairuz56@gmail.com>
2026-07-14 06:18:46 +0700

    Update postgres.py

--- database/postgres.py
+++ database/postgres.py
@@ -63,7 +63,7 @@ class PgConfig:
         host = os.getenv("PG_HOST", "").strip()
         port = int(os.getenv("PG_PORT", "6543"))
         database = os.getenv("PG_DATABASE", "postgres").strip()
-        ser = os.getenv("PG_USER", "").strip()
+        user = os.getenv("PG_USER", "").strip()
         password = os.getenv("PG_PASSWORD", "").strip()
 
         ssl_raw = os.getenv("PG_SSLMODE", "require").strip().lower()
