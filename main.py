1d13a62907abf6cbcfb04c7fa01e42535c0e6183
fayruzy4 <najibfairuz56@gmail.com>
2026-07-14 05:50:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import asyncio
 from telegram.ext import Application
 from config import BOT_TOKEN
 from features.keuangan.catat_keuangan import register
+from database.postgres import init_pool
 from features.habit.habit import post_init as habit_post_init
 
+async def startup(app):
+    init_pool()
+    await habit_post_init(app)
 def main():
     if not BOT_TOKEN:
         raise RuntimeError("TELEGRAM_BOT_TOKEN belum diisi.")
 
-    app = Application.builder().token(BOT_TOKEN).post_init(habit_post_init).build()
-
+    app = (
+    Application.builder()
+    .token(BOT_TOKEN)
+    .post_init(startup)
+    .build()
+    )
     from telegram.ext import MessageHandler, filters, ApplicationHandlerStop
     from config import OWNER_ID
 
